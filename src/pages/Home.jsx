47b1f2bc650179cbfc0f57550c3fc8ee902cc3fb
belot nicolas <nicolas-belot_student2021@wilder.school>
2022-02-04 21:51:42 +0100

    depart-navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../components/navbar/NavBar';
+
+import Img47 from '../assets/Img47.png';
+
+const Home = () => {
+  return (
+  <div className='Home'>
+      <NavBar />
+
+  </div>)
+};
+
+export default Home;
